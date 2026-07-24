edd4915689a69ca137f5424c8c72f197e978016d
Daksh agrawal <daksh.0832cs241058@cdgi.edu.in>
2026-07-24 21:41:42 +0530

    Update server/models/Stop.js

--- smart-bus-companion/server/models/Stop.js
+++ smart-bus-companion/server/models/Stop.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
 
 const stopSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: [true, 'Stop name is required'] },
   city: { type: String, default: 'Indore' },
   location: {
-    type: { type: String, enum: ['Point'], required: true },
-    coordinates: { type: [Number], required: true } // [longitude, latitude]
-  }
-});
+    type: { 
+      type: String, 
+      enum: { values: ['Point'], message: '{VALUE} is not supported' },
+      required: [true, 'Location type must be Point'] 
+    },
+    coordinates: { 
+      type: [Number], 
+      required: [true, 'Coordinates [lng, lat] are required'] 
+    } // [longitude, latitude]
+  },
+  deletedAt: { type: Date, default: null },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+}, { timestamps: true });
 
 stopSchema.index({ location: '2dsphere' });
 
